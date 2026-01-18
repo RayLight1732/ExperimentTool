23429d0f8747eafbea640d049762e8b5718f0381
RayLight <arusurobo@gmail.com>
2026-01-18 19:53:14 +0900

    update

--- usecase/unity_usecase.py
+++ usecase/unity_usecase.py
@@ -1,12 +1,9 @@
 from abc import ABC, abstractmethod
 from typing import Callable, Sequence
-from domain.entities.message import Message
 from domain.entities.connection_params import ConnectionParams
 
 
-class DeviceGateway(ABC):
-    """外部デバイスとの通信を抽象化するGatewayインターフェース"""
-
+class UnityUsecase(ABC):
     @abstractmethod
     def list_devices(self) -> Sequence[ConnectionParams]:
         """接続可能なデバイスの一覧を返す。"""
@@ -14,7 +11,7 @@ class DeviceGateway(ABC):
 
     @abstractmethod
     def connect(self, param: ConnectionParams) -> None:
-        """指定したデバイスに接続する（ポート名やアドレスなど実装依存）。"""
+        """指定したデバイスに接続する"""
         pass
 
     @abstractmethod
@@ -29,21 +26,30 @@ class DeviceGateway(ABC):
 
     @abstractmethod
     def on_disconnected(self, callback: Callable[[],]):
-        pass
-
-    @abstractmethod
-    def send_message(self, message: Message) -> None:
-        """
-        デバイスにメッセージを送信する。
-        """
-        pass
-
-    @abstractmethod
-    def on_message(self, callback: Callable[[Message], None]) -> None:
-        """受信時に呼ばれるコールバックを登録"""
+        """切断した際のコールバックを登録する"""
         pass
 
     @abstractmethod
     def is_connected(self) -> bool:
         """現在接続中かどうかを返す。"""
+        pass
+
+    @abstractmethod
+    def start(self) -> bool:
+        """実験を開始する"""
+        pass
+
+    @abstractmethod
+    def on_high_stress(self, callback: Callable[[],]):
+        """高負荷状態になった際のコールバックを登録する"""
+        pass
+
+    @abstractmethod
+    def on_low_stress(self, callback: Callable[[],]):
+        """低負荷状態になった際のコールバックを登録する"""
+        pass
+
+    @abstractmethod
+    def on_end_experiment(self, callback: Callable[[],]):
+        """実験が終わった際のコールバックを登録する"""
         pass
